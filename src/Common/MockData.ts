fbf9183116ef51d4cd8d8e9adf6342c07bc09c98
Benbenzhouz <zhouben168@gmail.com>
2024-08-24 12:49:16 +1000

    ben6
    ben6

---
+++ src/Common/MockData.ts
@@ -0,0 +1,16 @@
+export const HotSalesList=[
+    {Rank:1,DishName:"Stewed beef with potato",Like_Pc:98,Like_Qty:1002,Price:17.2},
+    {Rank:2,DishName:"Stewed beef with potato",Like_Pc:98,Like_Qty:1002,Price:17.2},
+    {Rank:3,DishName:"Stewed beef with potato",Like_Pc:98,Like_Qty:1002,Price:17.2},
+    {Rank:4,DishName:"Stewed beef with tomaaaato",Like_Pc:98,Like_Qty:1002,Price:17.2},
+    {Rank:5,DishName:"Stewed beef with potato",Like_Pc:98,Like_Qty:1002,Price:17.2},
+    {Rank:6,DishName:"Stewed beef with potato",Like_Pc:98,Like_Qty:1002,Price:17.2},
+]
+
+export const BookConfirmInfo = {
+    "DishName": "DemoData Sichuan Cuisine",
+    "Date": "27 Jun 2024",
+    "Time": "17:30",
+    "PeopleNum": "4",
+    "Introdution": "This is introduction. This is introduction. This is introduction. This is introduction. This is introduction. This is introduction. "
+}
